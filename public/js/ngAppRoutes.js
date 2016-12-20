4031588a3d9baf54261b1b2f665567afd5d8fb20
Jaya Bharadwaj <bharadwaj.j@gmail.com>
2016-12-21 02:53:32 +0530

    add footer link to show terms.

--- public/js/ngAppRoutes.js
+++ public/js/ngAppRoutes.js
@@ -11,7 +11,11 @@ mainNgApp.config(['$routeProvider', '$locationProvider', function($routeProvider
             templateUrl: 'templates/user-account-profile-tmpl.html',
             controller: 'gameUserCtrl'
         })
-        
+        .when('/footer', {
+            templateUrl: 'templates/footer-tmpl.html',
+            controller: 'gameFooterCtrl'
+        })
+
     $locationProvider.html5Mode(true);
 
 }]);
